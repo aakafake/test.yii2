ac47ba130047fa3a304b0a6e74f5e0e66cb1707c
aakafake <aakafake@mail.ru>
2017-04-23 20:28:25 +0300

    adminlte

--- basic/config/web.php
+++ basic/config/web.php
@@ -49,6 +49,14 @@ $config = [
             ],
         ],
 
+        'view' => [
+            'theme' => [
+                'pathMap' => [
+                    '@app/views' => '@vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app'
+                ],
+            ],
+        ],
+
     ],
     'params' => $params,
 ];
